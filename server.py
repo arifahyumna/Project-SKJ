3fc5039ea239d1bb397f2528060539f8403017e3
arifahyumna <you@example.com>
2026-05-07 07:58:42 +0700

    update server code:

--- server.py
+++ server.py
@@ -1,9 +1,26 @@
 import socket
+import sqlite3
 from datetime import datetime
 
-HOST = "0.0.0.0"   # biar bisa diakses dari ESP32
+HOST = "0.0.0.0"
 PORT = 5012
 
+# Koneksi Database SQLite
+conn_db = sqlite3.connect("sensor.db")
+cursor = conn_db.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS sensor_data (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    timestamp TEXT,
+    ip_address TEXT,
+    data TEXT
+)
+""")
+
+conn_db.commit()
+
+# Server TCP
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST, PORT))
 server.listen(5)
@@ -15,21 +32,30 @@ while True:
     print(f"Terhubung dari {addr}")
 
     data = b""
+
     while True:
         packet = conn.recv(1024)
+
         if not packet:
             break
+
         data += packet
 
-    # Tambahkan timestamp
+    # Timestamp
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    log = f"[{timestamp}] {addr} -> {data.decode()}\n"
 
-    # Simpan ke file
-    with open("data_sensor.txt", "a") as f:
-        f.write(log)
+    # Decode data
+    sensor_value = data.decode()
 
-    print("Data diterima:", data.decode())
+    # Simpan ke Database
+    cursor.execute("""
+    INSERT INTO sensor_data (timestamp, ip_address, data)
+    VALUES (?, ?, ?)
+    """, (timestamp, addr[0], sensor_value))
+
+    conn_db.commit()
+
+    print("Data diterima:", sensor_value)
 
     conn.close()
     print("Koneksi ditutup\n")
